df686d8ba72aff08a61c6b60496d3f3678259621
ddiresta <dominicdiresta@gmail.com>
2023-10-15 15:01:22 -0400

    Changes

--- housing_project.py
+++ housing_project.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('ZHVI.csv')
 
-print(df)
+df = df.dropna()
+
+df = df.transpose()
+
+df['Region'] = [None, 'Southeast', 'West', 'Southeast', 'Northeast', 'Southwest', 'Northeast', 'West', 'Northeast', 'West', 'Southwest', 'West', 'Southeast', 'Southeast', 'West', 'Midwest', 'West', 'West', 'Midwest', 'Southeast', 'Midwest', 'Southeast', 'Southeast', 'Northeast', 'Midwest', 'Northeast', 'Midwest', 'Midwest', 'Northeast', 'Northeast', 'Midwest', 'West', 'West', 'Northeast', 'Southwest', 'Southeast', 'Northeast', 'Southeast', 'Northeast', 'Midwest', 'Midwest', 'Midwest', 'West', 'Midwest', 'Midwest', 'Southeast', 'Southwest', 'Southeast', 'West', 'Southeast', 'Southeast', 'Northeast']
+
+df.to_csv('modified_ZHVI.csv', index=False)
